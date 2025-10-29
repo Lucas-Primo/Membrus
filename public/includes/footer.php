8e6d95bd1fa20810091ce6255742bb0a73aec1a7
Lucas-Primo <lucasgames3d@gmail.com>
2025-10-29 10:25:58 -0300

    Inciando a correção dos includes

---
+++ public/includes/footer.php
@@ -0,0 +1,19 @@
+<?php
+    echo'
+        <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./footer.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <footer>
+            <p>© 2024 Membrus. Todos os direitos reservados.</p>
+            <p>Desenvolvido por Equipe Metanoia.</p>
+        </footer>
+    </body>
+    </html>
+    ';
+?>
