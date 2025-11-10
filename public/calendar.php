8e20371f890055ac3aa336c25a4065f4c696ad88
Lucas-Primo <lucasgames3d@gmail.com>
2025-11-10 07:29:28 -0300

    Inicialização da organização das funções do CRUD

--- public/calendar.php
+++ public/calendar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/calendar.css">
-    <link rel="stylesheet" href="./css/global.css">
+    <!-- <link rel="stylesheet" href="./css/global.css"> -->
     <link rel="stylesheet" href="./includes/header.css">
     <link rel="stylesheet" href="./includes/sidebar.css">
 
